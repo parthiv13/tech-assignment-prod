e684fcc07895f2b062038be5f5b0fca30c7a459e
parthiv13 <kparthiv13@gmail.com>
2019-02-13 15:38:14 +0530

    Initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ app = express(),
 path = require('path'),
 port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist/sendtodevice/')));
+app.use(express.static(path.join(__dirname, 'dist/client/')));
 
 app.listen(port, () => {
     console.log(port)
